c9b735dc46d85d8c4594124557116d8fa885a178
Matt Walker <walkermatt@longwayaround.org.uk>
2017-12-12 08:18:24 +0000

    Read README from same directory as setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
+import os
+
+base_dir = os.path.dirname(__file__)
+with open(os.path.join(base_dir, "README.rst")) as f:
+    long_description = f.read()
 
 setup(
     name='spitslurp',
-    version='0.2',
+    version='0.3',
     description='Write text to a file with `spit` and read from a file with `slurp`.',
     author='Matt Walker',
     author_email='walkermatt@longwayaround.org.uk',
     url='https://github.com/walkermatt/spitslurp',
-    download_url='https://github.com/walkermatt/spitslurp/archive/0.2.tar.gz',
+    download_url='https://github.com/walkermatt/spitslurp/archive/0.3.tar.gz',
     packages=['spitslurp'],
     license='MIT',
     keywords=['io', 'file'],
-    long_description=open('README.rst').read()
+    long_description=long_description
 )
